9bf7f191e8031baea549f6ed814185a5fcb3a2c8
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-05-03 21:19:59 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/giancarloCavalli" target="_blank" rel="noreferrer">Giancarlo Cavalli</a><br/>
+                <small>Conheça mais sobre mim pelo linkedin: <a href="https://www.linkedin.com/in/giancarlo-cavalli-933385175/" target="_blank" rel="noreferrer">@giancarlo_c</a></small></p>
+                
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
