048e99ed6e7014c8819153bb93191a7706864593
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-05-07 22:01:21 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,6 +1,27 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { AmountBySeller } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[],
+    series: number[]
+}
 
 const DonutChart = () => {
+
+    //FORMA ERRADA
+    let chartData: ChartData = { labels: [], series: []};
+    
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response => {
+            const data = response.data as AmountBySeller[];
+            chartData.labels = data.map(x => x.sellerName);
+            chartData.series = data.map(x => x.sum);
+            console.log(chartData);
+        });
+
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
@@ -14,8 +35,8 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{ ...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="240"
         />
---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,4 @@
+export type AmountBySeller = {
+    "sellerName": string,
+    "sum": number
+}
